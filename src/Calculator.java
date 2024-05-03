02f738f1a6261af411210c5d0b3a6b8a87b515a6
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-05-03 12:32:27 +0200

    First test, it works

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Calculator {
-}
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+    public class Calculator implements ActionListener {
+        JFrame frame;
+        JTextField textfield;
+        JButton[] numberButtons = new JButton[10];
+        JButton[] functionButtons = new JButton[9];
+        JButton addButton,subButton,mulButton,divButton;
+        JButton decButton, equButton, delButton, clrButton, negButton;
+        JPanel panel;
+
+        Font myFont = new Font("INK FREE", Font.BOLD, 30);
+
+        double num1=0, num2=0,result=0;
+        char operator;
+
+
+        Calculator (){
+            frame = new JFrame("Calculator");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(500,550);
+            frame.setLayout(null);
+
+
+            frame.setVisible(true); // Move this line here
+        }
+
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+
