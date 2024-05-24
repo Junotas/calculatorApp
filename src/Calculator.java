874898afbf1b66a100a0c56c03cdbd4afbc71a8c
Simon Kendall <simon.a.kendall@gmail.com>
2024-05-24 16:03:08 +0200

    use a for loop to add buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -70,19 +70,11 @@ class Calculator implements ActionListener{
         panel.setBounds(50, 100, 300, 300);
         panel.setLayout(new GridLayout(4,4,10,10));
 
+        for(int i = 1; i < 10; i++) {
+            panel.add(numberButtons[i]);
+            if(i % 3 == 0) panel.add(functionButtons[i / 3 - 1]);
+        }
 
-        panel.add(numberButtons[1]);
-        panel.add(numberButtons[2]);
-        panel.add(numberButtons[3]);
-        panel.add(addButton);
-        panel.add(numberButtons[4]);
-        panel.add(numberButtons[5]);
-        panel.add(numberButtons[6]);
-        panel.add(subButton);
-        panel.add(numberButtons[7]);
-        panel.add(numberButtons[8]);
-        panel.add(numberButtons[9]);
-        panel.add(mulButton);
         panel.add(decButton);
         panel.add(numberButtons[0]);
         panel.add(equButton);
